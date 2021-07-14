767ebeb78585715ebc4cbeae789da607583e948d
duanweidong <duanweidong@meituan.com>
2021-07-15 00:40:39 +0800

    add HelloWorld!

---
+++ src/main/java/org/algorithms/TestMain.java
@@ -0,0 +1,12 @@
+package org.algorithms;
+
+/**
+ * @author duanweidong
+ * @version 2021/7/15 00:38
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
